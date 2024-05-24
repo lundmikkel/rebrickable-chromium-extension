8888a27b9e0d1ce690765aaac24470bfff3b821f
Frederik Martini <fma@cvation.com>
2024-05-24 10:58:35 +0200

    check for existing api key

--- src/popup.ts
+++ src/popup.ts
@@ -1,6 +1,43 @@
+import { SyncStorage } from "./storage/sync_storage";
+
 console.log("Popup script running");
 
+// TODO: get from somewhere else
+const syncStorage = new SyncStorage()
+
+const storageKey = "api-key"
+
 document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM content loaded")
+
+  registerPageContent()
+
+  registerSubmitApiKeyButton()
+
+  registerDeleteApiKeyButton()
+});
+
+function registerPageContent() {
+  const apiKey = syncStorage.Get(storageKey)
+
+  if (apiKey == "") {
+    hideDiv("enter-api-key-section")
+    showDiv("api-key-entered-section")
+  } else {
+    hideDiv("api-key-entered-section")
+    showDiv("enter-api-key-section")
+  }
+}
+
+function hideDiv(div: string) {
+  document.getElementById(div)!.hidden = true;
+}
+
+function showDiv(div: string) {
+  document.getElementById(div)!.hidden = false;
+}
+
+function registerSubmitApiKeyButton() {
   const button = document.getElementById("api-key-submit-button");
   if (button) {
     const input = <HTMLInputElement> document.getElementById("api-key-input-field")
@@ -15,9 +52,23 @@ document.addEventListener("DOMContentLoaded", () => {
       saveApiKey(key)
     });
   }
-});
+}
 
 function saveApiKey(key: string) {
-  //TODO: save in sync storage
-  alert(key);
+  syncStorage.Save(storageKey, key)
+}
+
+function registerDeleteApiKeyButton() {
+  const button = document.getElementById("api-key-delete-button");
+  if (button) {
+    button.addEventListener("click", () => {
+      deleteApiKey()
+
+      registerPageContent()
+    });
+  }
+}
+
+function deleteApiKey() {
+  syncStorage.Delete(storageKey)
 }
---
+++ src/storage/sync_storage.ts
@@ -0,0 +1,13 @@
+export class SyncStorage {
+    public Save(key: string, value: string) {
+        
+    }    
+
+    public Get(key: string): string | undefined {
+        return ""
+    }
+
+    public Delete(key: string) {
+        
+    }
+}
