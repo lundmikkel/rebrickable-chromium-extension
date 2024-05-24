1a5e79c688263caedfdd49e476e38ed5fc77a8bd
Frederik Martini <fma@cvation.com>
2024-05-24 10:16:00 +0200

    add simple input

--- src/popup.ts
+++ src/popup.ts
@@ -1,10 +1,23 @@
 console.log("Popup script running");
 
 document.addEventListener("DOMContentLoaded", () => {
-  const button = document.getElementById("action-button");
+  const button = document.getElementById("api-key-submit-button");
   if (button) {
+    const input = <HTMLInputElement> document.getElementById("api-key-input-field")
+
     button.addEventListener("click", () => {
-      alert("Button clicked!");
+      const key = input?.value;
+
+      if (!key) {
+        return;
+      }
+
+      saveApiKey(key)
     });
   }
 });
+
+function saveApiKey(key: string) {
+  //TODO: save in sync storage
+  alert(key);
+}
